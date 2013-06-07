82568018bb9419c2889de0823e02ce4002798ff0
Tuomas Starck <dev@starck.fi>
2013-06-07 15:32:05 +0300

    Adapt deployment script to change

--- deploy/githupdate.sh
+++ deploy/githupdate.sh
@@ -1,41 +1,35 @@
 #!/bin/sh
 
 ci_setup() {
-    DIR=`dirname $1`
+    DEPLOY=`dirname $1`
+    DIR=`dirname $DEPLOY`
+    INTENT=`basename $DIR`
     logger -t ci_upgrade -p local0.info "ci DIR: $DIR"
-    # SCRIPT=`basename $1`
-    # INTENT=`basename $DIR`
 }
 
 ci_upgrade() {
-    return
     logger -t ci_upgrade -p local0.info "ci: called"
     cd $DIR
     echo dir: $DIR
-    echo script: $SCRIPT
     echo intent: $INTENT
     git clean -f
     git reset --hard
     git pull
     git tag | xargs git tag -d
     git fetch --tags
-    logger -t ci_install -p local0.info "git did something"
+    echo git did something
 }
 
 ci_install() {
-    return
-    true
     logger -t ci_install -p local0.info "pwd at inst: `pwd`"
-    cat ${INTENT}.conf > /etc/init/${INTENT}.conf
+    cat $DEPLOY/${INTENT}.conf > /etc/init/${INTENT}.conf
+    chmod 644 /etc/init/${INTENT}.conf
     # also do something like
     echo git checkout tag and such such
 }
 
 ci_restart() {
-    return
-    true
-    # shutdown -r now
-    echo service stats restart
+    echo restarting...
     service $INTENT restart
     logger -t ci_install -p local0.info "ci: done."
 }
