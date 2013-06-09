f555d8ffedd283d2a616325412d044af4d2d8856
Tuomas Starck <dev@starck.fi>
2013-06-09 13:31:56 +0300

    Checkout and report correct version

--- deploy/githupdate.sh
+++ deploy/githupdate.sh
@@ -4,32 +4,36 @@ ci_setup() {
     DEPLOY=`dirname $1`
     DIR=`dirname $DEPLOY`
     INTENT=`basename $DIR`
-    logger -t ci_upgrade -p local0.info "ci DIR: $DIR"
+    HOST=`hostname`
+    RANK=`awk -vX=$HOST 'BEGIN{split(X,Y,"-");print Y[1]}'`
 }
 
 ci_upgrade() {
-    logger -t ci_upgrade -p local0.info "ci: called"
     cd $DIR
-    echo dir: $DIR
-    echo intent: $INTENT
+
+    git checkout master
     git clean -f
     git reset --hard
     git pull
     git tag | xargs git tag -d
     git fetch --tags
-    echo git did something
 }
 
 ci_install() {
-    logger -t ci_install -p local0.info "pwd at inst: `pwd`"
     cat $DEPLOY/${INTENT}.conf > /etc/init/${INTENT}.conf
     chmod 644 /etc/init/${INTENT}.conf
-    # also do something like
-    echo git checkout tag and such such
+
+    git tag -l | grep -q $RANK
+
+    if [ $? -eq 0 ]; then
+        git rev-parse ${RANK}~0 > $DIR/version
+        git checkout $RANK
+    else
+        echo error: cannot find a tag for the hostname
+        return 1
+    fi
 }
 
 ci_restart() {
-    echo restarting...
     service $INTENT restart
-    logger -t ci_install -p local0.info "ci: done."
 }
