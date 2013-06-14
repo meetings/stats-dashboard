cf1259a1a8a8464e0321fda0ed9a177d4a947f41
Tuomas Starck <dev@starck.fi>
2013-06-14 19:30:38 +0300

    Stop log flood

--- router.js
+++ router.js
@@ -1,13 +1,9 @@
 function route(handle,pathname){
-	console.log("About to route a request for " + pathname)
 	if(typeof handle[pathname] === 'function'){
 		return handle[pathname]();
 	} else {
-		console.log("No request handler found for " +pathname);
-		/*response.writeHead(404, {"Content-Type": "text/plain"});
-		response.write("404 Not found");
-		response.end();*/
 		return "404 Not found";
 	}
 }
+
 exports.route = route;
--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 var http = require("http");
-var url = require("url")
+var url = require("url");
 
 function start(route,handle){
 	function onRequest(request,response){
 		var pathname = url.parse(request.url).pathname;
-		console.log("Request for " + pathname + " received.");
 		var content = route(handle,pathname);
 		response.writeHead(200,{"Content-type": "text/plain"});
 		response.write(content);
